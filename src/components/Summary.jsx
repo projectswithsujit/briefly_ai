2e9996b1bb697ee75e5324cb2f373d4668c9a43c
projectswithsujit <projectswithsujit@gmail.com>
2026-05-28 15:53:47 +0545

    Update Summary.jsx

--- src/components/Summary.jsx
+++ src/components/Summary.jsx
@@ -25,6 +25,7 @@ export default function Summary({ content }) {
                     className="copy-btn"
                     onClick={() => {
                         navigator.clipboard.writeText(content);
+                        alert("Copied!");
                     }}
                 >
                     Copy Summary
